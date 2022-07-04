4ded63eee2dfa06d04e8c7fb266a8476e32dce28
Yauheni <eugene.kostroma@gmail.com>
2022-07-04 15:52:01 +0400

    feat: typing effect main section

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import ReactTypingEffect from 'react-typing-effect';
+
 import cs from '../../common/styles/Container.module.scss';
 
 import s from './Main.module.scss';
@@ -13,7 +15,7 @@ const Main: FC = () => {
       <Particle />
       <div className={`${cs.container} ${s.mainContainer}`}>
         <div className={s.greeting}>
-          <h3>Hello, I&apos;m</h3>
+          <ReactTypingEffect text="Hello, I'm" className={s.h3} />
           <h1>Yauheni Kastrama</h1>
           <h2>Frontend developer</h2>
         </div>
